e4847a27b17d9855edeb9905bd9db55023331d49
Rich Pauloo <richpauloo@gmail.com>
2020-10-30 15:42:07 -0700

    add water year types, range of seasonal high and low, and closest to 2015 measurement

--- code/gwl/01_gwl.R
+++ code/gwl/01_gwl.R
@@ -48,17 +48,21 @@ if(aoi_shp == "shp_sas/SAS Subbasin_B118.shp"){
   maoi <- maoi %>% filter(WSE > -200)
 }
 
+if(aoi_shp == "shp_ukiah/GSA_limit.shp"){
+  maoi <- maoi %>% filter(WSE > 300)
+}
+
 # sanity check: water levels
-ggplot(maoi, aes(MSMT_DATE, WSE, color = SWN)) + geom_line() + guides(color = FALSE)
-ggplot(maoi, aes(MSMT_DATE, -GSE_WSE, color = SWN)) + geom_line() + guides(color = FALSE)
+#ggplot(maoi, aes(MSMT_DATE, WSE, color = SWN)) + geom_line() + guides(color = FALSE)
+#ggplot(maoi, aes(MSMT_DATE, -GSE_WSE, color = SWN)) + geom_line() + guides(color = FALSE)
 
 # sanity check: mapview of wells
-mapview::mapview(maoi, zcol = "WELL_DEPTH") + 
-  mapview::mapview(aoi, alpha.regions = 0, lwd = 2, color = "red")
+#mapview::mapview(maoi, zcol = "WELL_DEPTH") + 
+#  mapview::mapview(aoi, alpha.regions = 0, lwd = 2, color = "red")
 
 # ggplots of DBGS - IMPT TO SORT!!!
 sc <- unique(maoi$SITE_CODE) %>% sort() 
-ns <- group_by(maoi, SITE_CODE) %>% slice(1) %>% arrange(SITE_CODE)
+ns <- group_by(maoi, SITE_CODE) %>% slice(1) %>% ungroup() %>% arrange(SITE_CODE) 
 ns$lab <- paste0("<p><b>state well num:</b> ", ns$SWN, "</p>",
                  "<p><b>coords x:</b> ", as_tibble(st_coordinates(ns))$X, "</p>",
                  "<p><b>coords y:</b> ", as_tibble(st_coordinates(ns))$Y, "</p>",
@@ -70,26 +74,100 @@ ns$lab <- paste0("<p><b>state well num:</b> ", ns$SWN, "</p>",
                  "<p><b>type:</b> ", ns$WELL_TYPE, "</p>",
                  "<p><b>agency:</b> ", ns$WLM_ORG_NAME, "</p>")
 
+# ------------------------------------------------------------------------
+# add seasons
+maoi <- maoi %>% 
+  mutate(
+    season = case_when(
+      month(MSMT_DATE) %in% 8:11 ~ "fall",
+      month(MSMT_DATE) %in% 3:5  ~ "spring",
+      TRUE ~ "other"
+    )
+  ) %>% 
+  # add seasonal range
+  group_by(SITE_CODE, season) %>% 
+  mutate(fall_high   = ifelse(season == "fall",   min(GSE_WSE, na.rm = TRUE), NA),
+         fall_low    = ifelse(season == "fall",   max(GSE_WSE, na.rm = TRUE), NA),
+         spring_high = ifelse(season == "spring", min(GSE_WSE, na.rm = TRUE), NA),
+         spring_low  = ifelse(season == "spring", max(GSE_WSE, na.rm = TRUE), NA)) %>% 
+  ungroup() %>% 
+  mutate(water_year = calculate_water_year(MSMT_DATE)) %>% 
+  left_join(wyt, by = c("water_year" = "WY")) %>% 
+  rename(water_year_type = `Yr-type`) %>% 
+  mutate(water_year_type = 
+           factor(water_year_type,
+                  levels = c("W","AN","BN","D","C"), 
+                  labels = c("Wet","Above Normal","Below Normal","Dry","Critical")),
+         water_year_start = ymd_hms(paste0(water_year, "-10-01 00:00:00")), 
+         water_year_end   = ymd_hms(paste0((water_year + 1), "-09-30 24:00:00")))
+
+# ------------------------------------------------------------------------
 # build plotly objects
 p <- vector("list", length(sc))
 for(i in seq_along(p)){
   d <- maoi %>% 
-    filter(SITE_CODE == sc[i]) 
+    filter(SITE_CODE == sc[i])
   
-  p[[i]] <- d %>% 
-    ggplot(aes(MSMT_DATE, -GSE_WSE)) +
-    geom_point() +
-    geom_line() +
-    labs(y = "DBGS (FT)", 
-         x = "Measurement date",
-         title = sc[i])
+  # closest to 2015-01-01
+  close_2015 <- 
+    tibble(MSMT_DATE = d[which.min(abs(d$MSMT_DATE - ymd_hms("2015-01-01 00:00:00"))), ]$MSMT_DATE,
+           close_Jan_2015 = d[which.min(abs(d$MSMT_DATE - ymd_hms("2015-01-01 00:00:00"))), ]$GSE_WSE)
+  
+  # water year range rectangles - join to WY type data
+  wy_rng <- c(min(d$water_year_start, na.rm = TRUE), 
+              max(d$water_year_start, na.rm = TRUE),
+              min(d$water_year_end, na.rm = TRUE), 
+              max(d$water_year_end, na.rm = TRUE))
+  wy_rect <- tibble(t0 = seq(wy_rng[1], wy_rng[2], "1 year"),
+                    t1 = seq(wy_rng[3], wy_rng[4], "1 year")) %>% 
+    mutate(water_year = year(t0)) %>% 
+    left_join(wyt, by = c("water_year" = "WY")) %>% 
+    rename(water_year_type = `Yr-type`) %>% 
+    mutate(water_year_type = 
+             factor(water_year_type,
+                    levels = c("W","AN","BN","D","C"), 
+                    labels = c("Wet","Above Normal","Below Normal","Dry","Critical")))
+  
+  # dumb hack to make ggplotly carry all factor levels
+  dummy_vals <- setdiff(levels(wy_rect$water_year_type), unique(wy_rect$water_year_type))
+  for(diff in seq_along(dummy_vals)) {
+    new_row_i <- nrow(wy_rect) + 1
+    wy_rect[new_row_i,] <- wy_rect[1,]
+    for (n in names(wy_rect[1,])) {
+      wy_rect[new_row_i,][n] = NA
+    }
+    wy_rect[new_row_i,]$water_year_type <- dummy_vals[diff]
+  }
+  
+  # build ggplots and plotly objects
+  p[[i]] <- ggplot() +
+    geom_rect(data = wy_rect, 
+              mapping = aes(
+                xmin  = t0, 
+                xmax  = t1, 
+                ymin  = min(-d$GSE_WSE, na.rm=TRUE), 
+                ymax  = 0,
+                fill  = water_year_type,
+                WY    = water_year), alpha = 0.5) +
+    geom_point(data = d, mapping = aes(MSMT_DATE, -GSE_WSE)) +
+    geom_line(data = d, mapping = aes(MSMT_DATE, -GSE_WSE)) +
+    geom_hline(data = close_2015, mapping = aes(yintercept = -close_Jan_2015), lwd = 1, color = "grey50") +
+    geom_hline(data = d, mapping = aes(yintercept = -spring_high), lwd = 1, linetype = "dotted", color = "blue") +
+    geom_hline(data = d, mapping = aes(yintercept = -spring_low),  lwd = 1, linetype = "dotted", color = "cyan") +
+    geom_hline(data = d, mapping = aes(yintercept = -fall_high),   lwd = 1, linetype = "dotted", color = "orange") +
+    geom_hline(data = d, mapping = aes(yintercept = -fall_low),    lwd = 1, linetype = "dotted", color = "red") +
+    scale_fill_brewer(palette = "RdYlBu", direction = -1, drop = FALSE) +
+    coord_cartesian(ylim = c(min(-d$GSE_WSE, na.rm=TRUE), 0)) +
+    labs(y = "DBGS (FT)", x = "Measurement date",
+         fill = "Water Year\nType", title = sc[i])
   p[[i]] <- ggplotly(p[[i]]) %>% 
     as.tags() %>%
-    {tags$div(style="width:270px; height:220px;", .)} %>%
+    {tags$div(style="width:800px; height:400px;", .)} %>%
     as.character() %>% 
     stringr::str_replace("height:400px","height:100%")
 }
 
+# ------------------------------------------------------------------------
 # leaflet
 pal <- colorNumeric(colormap::colormap(colormap::colormaps$viridis, nshades = 10), 
                     domain = ns$WELL_DEPTH)
@@ -107,7 +185,7 @@ l <- l %>%
                    stroke = FALSE,
                    radius = 4, 
                    fillOpacity = .8,
-                   popup = p, 
+                   popup = p,
                    label = lapply(ns$lab, htmltools::HTML)) %>% 
   addLegend(pal = pal, values = ns$WELL_DEPTH,
             title    = "Well depth (ft)",
@@ -126,7 +204,7 @@ l <- l %>%
   htmltools::attachDependencies(plotly:::plotlyMainBundle(), append = TRUE) %>%
   htmltools::attachDependencies(crosstalk::crosstalkLibs(),  append = TRUE) %>%
   browsable()
-l
+# l
 
 # write_rds(l, here("out", "leaf.rds"))
 
@@ -141,6 +219,7 @@ dt <- ns %>%
 # subset to the site codes
 n <- unique(maoi$SITE_CODE)
 
+# ------------------------------------------------------------------------
 # write csvs - first arrange data and add geometry
 df <- as(maoi, "Spatial")@data %>% 
   select(-c("STN_ID.x", "STN_ID.y")) %>% 
--- code/gwl/03_control.R
+++ code/gwl/03_control.R
@@ -8,8 +8,17 @@ library(htmltools)
 library(htmlwidgets)
 library(leafem)
 library(here)
+library(lubridate)
 
-# relational tables in periodic grroundwater level measurement database
+# find water year given year
+calculate_water_year <- function(date){
+  yr = lubridate::year(date)
+  mh = lubridate::month(date)
+  yr = ifelse(mh %in% 1:9, yr - 1, yr)
+  return(yr)
+}
+
+# relational tables in periodic groundwater level measurement database
 m    <- read_csv(here("in", "periodic_gwl_bulkdatadownload", "measurements.csv"))
 perf <- read_csv(here("in", "periodic_gwl_bulkdatadownload", "perforations.csv"))
 sta  <- read_csv(here("in", "periodic_gwl_bulkdatadownload", "stations.csv"))
@@ -21,6 +30,9 @@ m <- left_join(m, perf, by = "SITE_CODE") %>%
 # make spatial
 m <- st_as_sf(m, coords = c("LONGITUDE","LATITUDE"), crs = 4269)
 m <- as(m, "Spatial")
+
+# water year type data
+wyt <- read_csv(here("in", "sac_water_year_types.csv"))
 
 # write dashboards
 write_dashboard <- function(x) {
